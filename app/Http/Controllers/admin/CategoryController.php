8c7d2a4fa7fc88cdc7acb50395ea08d11687b7a7
NTNgoc <ngocfaker@gmail.com>
2019-05-23 21:20:44 +0700

    CRUD category

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -13,30 +13,26 @@ class CategoryController extends Controller
     public function listCategory()
     {
     	$categories=Category::orderBy('id','ASC')->get();
-<<<<<<< HEAD
+
 
     	return view('admin.category.listCategory',compact('categories'));
     }
 
-=======
+
     	return view('admin.category.listCategory',compact('categories'));
     }
->>>>>>> d122f77fbc7cdef5dcba7f48b715a0e0f5f528ca
+
     public function addViewCategory()
     {
     	return view('admin.category.addCategory');
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> d122f77fbc7cdef5dcba7f48b715a0e0f5f528ca
     public function addProcessCategory(Request $request)
     {
     	$categories = new Category();
     	$categories->name =$request->nameCategory;
     	$categories->slug='';
     	$categories->save();
-<<<<<<< HEAD
 
     	return redirect()->route('categories');
     }
@@ -48,21 +44,20 @@ class CategoryController extends Controller
     	return view('admin.category.updateCategory',compact('categories'));
     }
 
-=======
     	return redirect()->route('categories');
     }
+
     public function updateViewCategory($id)
     {
     	$categories=Category::find($id);
     	return view('admin.category.updateCategory',compact('categories'));
     }
->>>>>>> d122f77fbc7cdef5dcba7f48b715a0e0f5f528ca
+
     public function updateProcessCategory(Request $request,$id)
     {
     	$categories = new Category();
     	$name=$categories->name=$request->nameCategory;
     	$categories=Category::where('id',$id)->update(['name'=>$name]);
-<<<<<<< HEAD
 
     	return redirect()->route('categories');
     }
@@ -71,13 +66,12 @@ class CategoryController extends Controller
     {
     	Category::where('id',$id)->delete();
 
-=======
     	return redirect()->route('categories');
     }
     public function deleteCategory($id)
     {
     	Category::where('id',$id)->delete();
->>>>>>> d122f77fbc7cdef5dcba7f48b715a0e0f5f528ca
+
     	return redirect()->route('categories');
     }
 }
