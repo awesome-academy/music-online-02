76c4d92d34a876f9b61a21c2d2a3393ad14dfc55
NTNgoc <ngocfaker@gmail.com>
2019-05-31 12:25:00 +0700

    CRUD Category

---
+++ resources/views/admin/category/addCategory.blade.php
@@ -0,0 +1,20 @@
+@extends('admin.master')
+@section('content')
+    <form action="{{ route('categories.add_process') }}" method="post">
+	    <legend>{{ trans('label.home_category') }}</legend>
+	    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+		<table class="table"> 
+		    <tr>
+				<td>
+					<div class="form-group col-8">
+					    <label for="">{{ trans('label.add_category') }}</label>
+				        <input type="text" name="nameCategory" class="form-control" placeholder="{{ trans('label.name') }}">
+					</div>
+				</td>
+			</tr>
+			</tr>
+				<td colspan="2" align="center"><input type="submit" value="{{ trans('label.add') }}"></td>
+			</tr>
+		</table>
+    </form>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,39 @@ use App\Http\Controllers\PageController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::group(['prefix' => 'admin'],function()
+{
+    Route::get('/',[
+        'as' => 'home_ad.',
+        'uses' => 'admin\HomeController@home'
+    ]);
+    Route::get('/categories',[
+        'as' => 'categories',
+        'uses' => 'admin\CategoryController@listCategory'
+    ]);
+    Route::get('/addcategories',[
+        'as' => 'categories.add_view',
+        'uses' => 'admin\CategoryController@addViewCategory'
+    ]);
+    Route::post('/addProcessCategory',[
+        'as' => 'categories.add_process',
+        'uses' => 'admin\CategoryController@addProcessCategory'
+    ]);
+    Route::get('updateCategory/{id}',[
+        'as' => 'categories.update_view',
+        'uses' => 'admin\CategoryController@updateViewCategory'
+    ]);
+    Route::post('updateProcessCategory/{id}',[
+        'as' => 'categories.update_process',
+        'uses' => 'admin\CategoryController@updateProcessCategory'
+    ]);
+    Route::get('deleteCategory/{id}',[
+        'as' => 'categories.delete',
+        'uses' => 'admin\CategoryController@deleteCategory'
+    ]);
+}
+);
+////////////
 Auth::routes();
 Route::get('/', 'PageController@index');
 Route::get('music/{id}/{slug}', 'MusicController@index');
